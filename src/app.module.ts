1677fee97d95d2dd19f667de0d5dc5fbdbb5219c
mikhail-svekolnikov1 <mikhail@gifted.co>
2025-11-14 21:53:00 +0200

    Nest - create resource tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { SlackModule } from './slack/slack.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [AuthModule, SlackModule],
+  imports: [AuthModule, SlackModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
