178e88197773497473dcbf615c9e2381af04b994
mikhail-svekolnikov1 <mikhail@gifted.co>
2025-11-15 11:13:23 +0200

    Nest - Connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { SlackModule } from './slack/slack.module';
 import { TasksModule } from './tasks/tasks.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [AuthModule, SlackModule, TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true, // автоматически подключать все сущности
+      synchronize: true, // создаёт таблицы автоматически (не использовать в prod)
+    }),
+    AuthModule,
+    SlackModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
